17debf5cb6c6e62c8ed7381dda8b6315c140c13f
Pietro <pietrotoldo1@gmail.com>
2025-06-23 22:10:18 -0300

    backup error

--- src/pages/Relatorio/Relatorio.jsx
+++ src/pages/Relatorio/Relatorio.jsx
@@ -1,3 +1,55 @@
+import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import {
+  Wrapper,
+  Container,
+  Title,
+  IconWrapper,
+  CardContainer,
+  Card,
+  CardIcon,
+  CardTitle,
+  GenerateButton
+} from './Style';
+
+import { FaFileAlt, FaCarrot, FaCalendarAlt } from 'react-icons/fa';
+
 const Relatorio = () => {
-  return <div>Teste Relatório</div>;
+  return (
+    <Wrapper>
+      <Header />
+
+      <Container>
+        <IconWrapper>
+          <FaFileAlt size={60} color="#3b82f6" />
+          <Title>RELATÓRIOS</Title>
+        </IconWrapper>
+
+        <CardContainer>
+          <Card>
+            <CardIcon><FaFileAlt size={32} /></CardIcon>
+            <CardTitle>RELATÓRIO DE MOVIMENTAÇÃO</CardTitle>
+            <GenerateButton>GERAR</GenerateButton>
+          </Card>
+
+          <Card>
+            <CardIcon><FaCarrot size={32} /></CardIcon>
+            <CardTitle>INGREDIENTES UTILIZADOS</CardTitle>
+            <GenerateButton>GERAR</GenerateButton>
+          </Card>
+
+          <Card>
+            <CardIcon><FaCalendarAlt size={32} /></CardIcon>
+            <CardTitle>RELATÓRIO POR PERÍODO</CardTitle>
+            <GenerateButton>GERAR</GenerateButton>
+          </Card>
+        </CardContainer>
+      </Container>
+
+      <Footer />
+    </Wrapper>
+  );
 };
+
+export default Relatorio;
